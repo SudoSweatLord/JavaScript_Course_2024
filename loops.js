4f0cf351668ff28f27d9cdf28d31afd900322a9f
K-Fahrenberger <fahrenberger.k@web.de>
2025-01-28 14:26:03 +0100

    add: comment about when to use

--- loops.js
+++ loops.js
@@ -1,10 +1,10 @@
-//for loop
+//for loop iterating over an array
 let numbers = [1, 2, 3, 4, 5, 6, 7];
 for (let idx = 0; idx < numbers.length; idx++) {
   console.log(numbers[idx]);
 }
 
-//while loop
+//while loop execute same block of code whilst a condition is true
 let idx = 0;
 while (idx < numbers.length) {
   console.log(numbers[idx]);
