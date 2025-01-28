4a2b7c7ec37cc57b5c92d4db5e725d919dc9343b
K-Fahrenberger <fahrenberger.k@web.de>
2025-01-28 13:00:09 +0100

    add: Ternary; Precedence of Operators

---
+++ operatorsPrecedence.js
@@ -0,0 +1,4 @@
+let n = 5 + 5 * 4;
+console.log(n);
+
+//order for Operators is Parenthesis, Exponents, Multiplication, Division, Addition, Subtraction
---
+++ operatorsTernary.js
@@ -0,0 +1,6 @@
+//example
+const result = "test" === "test" ? true : false;
+
+//example 2
+let points = 110;
+const customerType = points > 100 ? "gold" : "silver";
