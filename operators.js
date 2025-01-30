e34a8e71b17c7f0c8eace5700d357fc9a5c7fbe1
K-Fahrenberger <fahrenberger.k@web.de>
2025-01-30 13:29:14 +0100

    add: loops; operators

--- operators.js
+++ operators.js
@@ -4,19 +4,19 @@
 // ?? null coalescing
 
 //Or operator
-console.log(true || true);//true
-console.log(false || true);//true
-console.log(true || false);//true
-console.log(false || false);//false
+console.log(true || true); //true
+console.log(false || true); //true
+console.log(true || false); //true
+console.log(false || false); //false
 
 //And operator
-console.log(true && true);//true
-console.log(false && true);//false
-console.log(true && false);//false
-console.log(false && false);//false
+console.log(true && true); //true
+console.log(false && true); //false
+console.log(true && false); //false
+console.log(false && false); //false
 
 //Null coalescing operator
-console.log(true ?? true);//false
-console.log(false ?? true);//true
-console.log(true ?? false);//false
-console.log(false ?? false);//true
+console.log(true ?? true); //false
+console.log(false ?? true); //true
+console.log(true ?? false); //false
+console.log(false ?? false); //true
