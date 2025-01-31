85821d86e6d4ee7e3ab2ec9037d544cfb00603e1
K-Fahrenberger <fahrenberger.k@web.de>
2025-01-31 20:06:52 +0100

    fix: name of switch Case file

---
+++ repeat.js
@@ -0,0 +1,13 @@
+//Ternary Operator
+
+const result = "test" === "test" ? true : false;
+console.log(result);
+
+//Switch statements
+// only usesd for equality comparison
+
+let job = "Software Developer";
+
+if (job === "Software Developer") {
+  console.log("writes code");
+}
