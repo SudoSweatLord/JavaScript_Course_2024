4eed0ddcd9abb77e48860f9cc64eec7cb99d3139
K-Fahrenberger <fahrenberger.k@web.de>
2025-02-11 11:49:36 +0100

    add: 11

---
+++ 11_Checked_Property/index.js
@@ -0,0 +1,24 @@
+const myCheckBox = document.getElementById("myCheckBox");
+const visaBtn = document.getElementById("visaBtn");
+const masterCardBtn = document.getElementById("masterCardBtn");
+const payPalBtn = document.getElementById("payPalBtn");
+const mySubmitBtn = document.getElementById("mySubmitBtn");
+const subResult = document.getElementById("subResult");
+const paymentResult = document.getElementById("paymentResult");
+
+mySubmit.onclick = function () {
+  if (myCheckBox.checked) {
+    subResult.textContent = `You are subscribed`;
+  } else {
+    subResult.textContent = `You are not subscribed`;
+  }
+  if(visaBtn.checked){
+    paymentResult.textContent = `You are paying with Visa`
+  } else if(masterCardBtn.checked){
+    paymentResult.textContent = `You are paying with Master Card`
+  } else if(payPalBtn.checked) {
+    paymentResult.textContent = `You are paying with PayPal`
+  } else {
+    paymentResult.textContent = `You haven't selected a payment method yet`
+  }
+};
