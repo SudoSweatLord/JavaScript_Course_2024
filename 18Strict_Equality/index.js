2eaf5d06f3b3bef3b5bae05a36a193c2b89365c4
K-Fahrenberger <fahrenberger.k@web.de>
2025-02-11 15:47:58 +0100

    add: 18

---
+++ 18Strict_Equality/index.js
@@ -0,0 +1,17 @@
+/**
+ * = assignment operator
+ * == comparison operator
+ * === strict equality operator
+ * != inequality operator
+ * !== strict inequality operator
+ */
+
+const PI = 3.14;
+
+if (PI === 3.14) {
+  console.log("That is Pi");
+} else {
+  console.log("That is not Pi");
+}
+
+//strict === is superior
