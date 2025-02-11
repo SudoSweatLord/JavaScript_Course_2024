76a0340b81a0f55225776a2a3b1372d317388ae1
K-Fahrenberger <fahrenberger.k@web.de>
2025-02-11 15:38:41 +0100

    add: 17

---
+++ 17Logical_Operators/index.js
@@ -0,0 +1,9 @@
+const temp = 20;
+
+if (temp > 0 && temp < 30) {
+  console.log("the weather is good");
+} else if (temp <= 30) {
+  console.log("the weather is good");
+} else {
+  console.log("the weather is bad");
+}
