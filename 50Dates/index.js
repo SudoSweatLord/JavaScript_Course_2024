286153c66ee5845acb7427936c7648ed3875c455
K-Fahrenberger <fahrenberger.k@web.de>
2025-02-15 15:41:29 +0100

    add: 50

---
+++ 50Dates/index.js
@@ -0,0 +1,26 @@
+// const date = new Date(2024, 1, 1, 2, 3, 4, 5);
+// console.log(date);
+
+// const year = date.getFullYear();
+// const month = date.getMonth();
+// const hour = date.getHours();
+// console.log(month);
+// console.log(year);
+// console.log(hour);
+
+// const date = new Date();
+
+// date.setFullYear(2024);
+// date.setMonth(1);
+// date.setDate(1);
+// date.setHours(4);
+// date.setMinutes(55);
+// date.setSeconds(38);
+// console.log(date)
+
+const date1 = new Date("2023-12-31");
+const date2 = new Date("2024-01-01");
+//comparable
+if (date1 < date2) {
+  console.log("date 1 is bigger");
+}
