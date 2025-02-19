0fd3ea3d41dd11cfa5644b003fca5ff71277bead
K-Fahrenberger <fahrenberger.k@web.de>
2025-02-19 16:24:28 +0100

    add: 65

---
+++ 65Hide_show_HTML/index.js
@@ -0,0 +1,12 @@
+const myButton = document.getElementById("myButton");
+const myImage = document.getElementById("myImage");
+
+myButton.addEventListener("click", (event) => {
+  if (myImage.style.visibility === "hidden") {
+    myImage.style.visibility = "visible";
+    myButton.textContent = "Hide";
+  } else {
+    myImage.style.visibility = "hidden";
+    myButton.textContent = "Show";
+  }
+});
