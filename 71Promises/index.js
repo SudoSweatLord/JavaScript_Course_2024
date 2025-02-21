08e317b1eaa5b187e428e95d8a5e4d3284a0d52b
K-Fahrenberger <fahrenberger.k@web.de>
2025-02-21 17:50:02 +0100

    check

---
+++ 71Promises/index.js
@@ -0,0 +1,6 @@
+// Promise = An Object that manages asynchronous operations.
+// Wrap a Promise object around {asynchronous code}
+// "I promise to return a value"
+// PENDING ->((resolve, reject) => {asynchronous code})
+
+// Do these chores in order
