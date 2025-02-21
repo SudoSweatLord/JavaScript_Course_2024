08dce2d69ea045db8bede6b7344ec5d5d3411506
K-Fahrenberger <fahrenberger.k@web.de>
2025-02-21 19:27:48 +0100

    add: stage

---
+++ 72Asynch_Await/index.js
@@ -0,0 +1,6 @@
+// Async/Await = Asynch = makes a function return a promise
+// Await = makes an asynch function wait for a promise
+
+// Allows you to write asynchronous code in a synchronous manner
+// Async doesn't have resolve or reject parameters
+// Everything after Await is placed in an event queue
