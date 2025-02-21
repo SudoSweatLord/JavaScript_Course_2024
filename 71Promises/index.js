535739be99619a66afe3466558c6948965c1cea9
K-Fahrenberger <fahrenberger.k@web.de>
2025-02-21 18:39:50 +0100

    add: 71

--- 71Promises/index.js
+++ 71Promises/index.js
@@ -4,3 +4,49 @@
 // PENDING ->((resolve, reject) => {asynchronous code})
 
 // Do these chores in order
+// 1. Walk dog
+// 2. Clean Kitchen
+// 3. Take out the trash
+
+function walkDog() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const dogwalked = true;
+      if (dogwalked) {
+        resolve("You walked the dog");
+      } else {
+        reject("You didn't walk the dog");
+      }
+    }, 1500);
+  });
+}
+
+function cleanKitchen() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("You clean the kitchen");
+    }, 2500);
+  });
+}
+
+function takeOutTrash() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("You take out the trash");
+    }, 500);
+  });
+}
+
+walkDog()
+  .then((value) => {
+    console.log(value);
+    return cleanKitchen();
+  })
+  .then((value) => {
+    console.log(value);
+    return takeOutTrash();
+  })
+  .then((value) => {
+    console.log(value);
+    console.log("finished");
+  });
