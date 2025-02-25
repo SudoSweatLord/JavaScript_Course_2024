202ad1248b6ef586d4dc6179733a850ae8f44ae3
K-Fahrenberger <fahrenberger.k@web.de>
2025-02-25 09:45:16 +0100

    add: 72

--- 72Asynch_Await/index.js
+++ 72Asynch_Await/index.js
@@ -4,3 +4,50 @@
 // Allows you to write asynchronous code in a synchronous manner
 // Async doesn't have resolve or reject parameters
 // Everything after Await is placed in an event queue
+
+function walkDog() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const dogwalked = true;
+      if (dogwalked) {
+        resolve("You walked the dog");
+      } else {
+        reject("You didn't walk the dog");
+      }
+    }, 1500);
+  });
+}
+
+function cleanKitchen() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("You clean the kitchen");
+    }, 2500);
+  });
+}
+
+function takeOutTrash() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("You take out the trash");
+    }, 500);
+  });
+}
+//add try and catch in case of errors
+
+async function doChores() {
+  try {
+    const walkDogResult = await walkDog();
+    console.log(walkDogResult);
+    const cleanKitchenResult = await cleanKitchen();
+    console.log(cleanKitchenResult);
+    const takeOutTrashResult = await takeOutTrash();
+    console.log(takeOutTrashResult);
+
+    console.log("you finished all the chores");
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+doChores();
